04a369f50f063be3b43d4e19796eaad1267c1f50
naveen kumar <naveen.lingampelli21@gmail.com>
2025-10-24 10:59:48 +0530

    k8

---
+++ 70-frontend_alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z05123311NS9PJF4CG0SG"
+}
+
+variable "zone_name" {
+    default = "devops84.store"
+}
